94ba97c4044612dc6c3b177fff32c809d617c8fb
Ashwin K Whitchurch <ashwin@protocentral.com>
2020-05-24 16:37:32 +0530

    Added healthypi installation module

---
+++ stage4/04-healthypi/00-run.sh
@@ -0,0 +1,39 @@
+#!/bin/sh -e
+
+# Delete this in production
+#ROOTFS_DIR="/Users/akw/Documents"
+
+healthypi_loc="https://github.com/Protocentral/protocentral_healthypi_v4/releases/latest/download/healthypi-rpi.zip"
+
+wget "$healthypi_loc" -O "files/healthypi.zip"
+
+rm -r {ROOTFS_DIR}/HealthyPi ||:
+rm -r files/application.linux-armv6hf ||:
+unzip files/healthypi.zip -d files/
+mv files/application.linux-armv6hf/* files/
+rm -r files/application.linux-armv6hf
+
+#mkdir ${ROOTFS_DIR}/HealthyPi
+install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi"
+install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/lib"
+install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/data"
+
+install -m 777 "files/gui" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/gui"
+chmod +x ${ROOTFS_DIR}/HealthyPi/gui
+
+for file in files/lib/*;do
+    install -m 755 "$file" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/lib"
+done
+
+for file in files/data/*;do
+    install -m 755 "$file" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/data"
+done
+
+#sed -i -e "${ROOTFS_DIR}\/home\/${FIRST_USER_NAME}\/HealthyPi\/gui/d" ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
+echo "@/home/${FIRST_USER_NAME}/HealthyPi/gui &" >> ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
+
+
+#install -v -o 1000 -g 1000 -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi"
+#install -v -o 1000 -g 1000 -m 644 "files/$magpi_latest" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/"
+
+#install -v -o 1000 -g 1000 -m 644 "files/$magpi_latest" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/MagPi/"
