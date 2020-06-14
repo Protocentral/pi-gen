d0d81e604ffbd2be180dfc9d2dd9f54e7b4c9db1
Ashwin K Whitchurch <ashwin@protocentral.com>
2020-06-14 20:19:34 +0530

    Corrected HealthyPi GUI installation

--- stage4/04-healthypi/00-run.sh
+++ stage4/04-healthypi/00-run.sh
@@ -1,8 +1,5 @@
 #!/bin/sh -e
 
-# Delete this in production
-#ROOTFS_DIR="/Users/akw/Documents"
-
 healthypi_loc="https://github.com/Protocentral/protocentral_healthypi_v4/releases/latest/download/healthypi-rpi.zip"
 
 wget "$healthypi_loc" -O "files/healthypi.zip"
@@ -14,25 +11,29 @@ mv files/application.linux-armv6hf/* files/
 rm -r files/application.linux-armv6hf
 
 #mkdir ${ROOTFS_DIR}/HealthyPi
-install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi"
-install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/lib"
-install -v -d "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/data"
+install -v -o 1000 -g 1000 -d  "${ROOTFS_DIR}/opt/HealthyPi"
+install -v -o 1000 -g 1000 -d  "${ROOTFS_DIR}/opt/HealthyPi/lib"
+install -v -o 1000 -g 1000 -d  "${ROOTFS_DIR}/opt/HealthyPi/data"
 
-install -m 777 "files/gui" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/gui"
-chmod +x ${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/gui
+install -m 777 "files/gui" "${ROOTFS_DIR}/opt/HealthyPi/gui"
+
+chmod +x ${ROOTFS_DIR}/opt/HealthyPi/gui
 
 for file in files/lib/*;do
-    install -m 755 "$file" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/lib"
+    install -v -o 1000 -g 1000 -d -m 755 "$file" "${ROOTFS_DIR}/opt/HealthyPi/lib"
 done
 
 for file in files/data/*;do
-    install -m 755 "$file" "${ROOTFS_DIR}/home/${FIRST_USER_NAME}/HealthyPi/data"
+    install -v -o 1000 -g 1000 -d  -m 755 "$file" "${ROOTFS_DIR}/opt/HealthyPi/data"
 done
 
-#sed -i -e "${ROOTFS_DIR}\/home\/${FIRST_USER_NAME}\/HealthyPi\/gui/d" ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
-echo "sudo @/home/${FIRST_USER_NAME}/HealthyPi/gui &" >> ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
+sed -i -e '\/opt\/HealthyPi\/gui/d' ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
 
-#rm "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
+echo "@/opt/HealthyPi/gui &" >> ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
+
+cat ${ROOTFS_DIR}/etc/xdg/lxsession/LXDE-pi/autostart
+
+rm "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
 install -v -m 644 "files/proto.jpg" "${ROOTFS_DIR}/usr/share/rpd-wallpaper/temple.jpg"
 sed -i '/console=serial0,115200/d' "${ROOTFS_DIR}/boot/cmdline.txt"
 
